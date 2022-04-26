9dc6c117fe89274b52e87d984933a8e9cd555230
DylanCOSTACODER <gueshinho49@gmail.com>
2022-04-26 08:01:03 +0200

    first commit

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Product;
+
+class ProductFactory extends Factory
+{
+
+    protected $model = Product::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->text(30),
+            'description' => $this->faker->text,
+            'image' => $this->faker->imageUrl(),
+            'price' => $this->faker->numberBetween(10, 100)
+        ];
+    }
+}
